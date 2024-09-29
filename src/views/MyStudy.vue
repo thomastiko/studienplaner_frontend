c06de792182174980dfb7283a3c3fbff25585553
thomastiko <92720511+thomastiko@users.noreply.github.com>
2024-09-29 16:10:44 +0200

    added myStudy card for user

--- src/views/MyStudy.vue
+++ src/views/MyStudy.vue
@@ -3,6 +3,18 @@
     <div class="text-h1 text-center text-weight-medium text-uppercase q-mb-sm">
       Mein Studium
     </div>
+    <div class="column items-center">
+      <div class="col" v-for="(study, i) in this.userStore.user.studies" :key="i" >
+        <q-card class="my-card cursor-pointer" v-ripple>
+      <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+        <div class="absolute-bottom">
+          <div class="text-h4 text-white"> {{study.study_name_short}} </div>
+          <div class="text-subtitle1">{{study.study_name}}</div>
+        </div>
+      </q-img>
+    </q-card>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,14 +28,23 @@ export default {
         userStore,
     };
   },
-  /* async created() {
-    if (!this.userStore.loggedIn) {
-      this.$router.push("/");
-    }
-  },*/
   methods: {},
   computed: {},
+  mounted() {
+    console.log(this.userStore.user)
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.my-card {
+  min-width: 350px;
+  max-width: 400px;
+  width: 100%;
+  transition: all 0.3s;
+}
+
+.my-card:hover {
+  filter: brightness(1.1);
+}
+</style>
