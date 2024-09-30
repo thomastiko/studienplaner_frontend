752aa9e170a15e6350752d933bea9cba97d2fd3b
thomastiko <92720511+thomastiko@users.noreply.github.com>
2024-09-30 21:10:04 +0200

    added studyplan structure and subject structure

---
+++ src/components/studyplaner/subject.vue
@@ -0,0 +1,47 @@
+<template>
+<q-card class="my-card">
+      <q-card-section :style="{backgroundColor: getSubjectColor()}">
+        <div class="text-h6 text-grey-9"> {{subject.name}} </div>
+        <div class="text-subtitle2"> {{subject.category}} </div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions align="right">
+        <q-btn flat>Action 1</q-btn>
+        <q-btn flat>Action 2</q-btn>
+      </q-card-actions>
+    </q-card>
+</template>
+
+<script>
+export default {
+  props: {
+    subject: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    getSubjectColor() {
+      let color = this.subject.color
+      if (this.subject.status == "unavailable") {
+        color = "#D9D9D9"
+      } else if (this.subject.status == "done") {
+        color = "#98F280"
+      } else if (this.subject.status == "doing") {
+        color = "#FFA03D"
+      } else {
+        color = this.subject.color
+      }
+      return color || "#FFFFFF";
+    }
+  },
+  mounted() {
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/StudyPlan.vue
+++ src/views/StudyPlan.vue
@@ -1,31 +1,72 @@
 <template>
   <div>
-    {{ this.userStore.user.studies }}
+    <div v-if="selectedStudy">
+      <!-- Iteration durch Phasen und die Fächer, die zu diesen Phasen gehören -->
+      <div v-for="(subjects, phase) in groupedSubjects" :key="phase" class="col-12 shadow-1 q-ma-md">
+        <div class="q-pa-md">
+          <div class="col-12 row justify-between items-center">
+          <div class="text-h5 text-bold">{{ phase }}</div>
+          <div> {{subjects.length}} Lehrveranstalungen </div>
+          </div>
+          <div v-for="subject in subjects" :key="subject._id">
+            <Subject :subject="subject" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      <p>Study not found.</p>
+    </div>
   </div>
 </template>
 
 <script>
-import { useUserStore } from '@/stores/user.store';
+import { useUserStore } from '@/stores/user.store'
+import Subject from '../components/studyplaner/subject.vue'
+import { ref } from 'vue'
 export default {
+  components: {
+    Subject
+  },
   props: ['study_id'],
   setup(props) {
-    let studyId = props.study_id || window.location.pathname.split("/")[2];
+    let studyId = ref(props.study_id) || window.location.pathname.split('/')[2]
 
-    if (!studyId) {
-      throw new Error("No study id provided");
+    if (!studyId.value) {
+      throw new Error('No study id provided')
     }
 
-    
-    const userStore = useUserStore();
-    console.log(props.study_id);
+    const userStore = useUserStore()
     return {
-        userStore,
-    };
+      userStore
+    }
   },
+  data() {
+    return {
+      studyId: this.study_id
+    }
+  },
+  computed: {
+    selectedStudy() {
+      return this.userStore.user.studies.find((study) => study.study_id === this.studyId)
+    },
+    groupedSubjects() {
+      if (!this.selectedStudy || !this.selectedStudy.subject_states) {
+        return {}
+      }
 
+      // Gruppieren der subject_states nach Phase
+      return this.selectedStudy.subject_states.reduce((groups, subject) => {
+        const phase = subject.phase
+        if (!groups[phase]) {
+          groups[phase] = []
+        }
+        groups[phase].push(subject)
+        return groups
+      }, {})
+    }
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
