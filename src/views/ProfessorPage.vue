c2a72c7dc38266579a3842e4aa73f9071a195234
thomastiko <92720511+thomastiko@users.noreply.github.com>
2024-12-21 23:12:51 +0100

    //Feedback codeq: ProfessorPage Design überarbeitet

--- src/views/ProfessorPage.vue
+++ src/views/ProfessorPage.vue
@@ -1,161 +1,167 @@
 <template>
   <div>
     <div class="row q-ma-md" v-if="profStore.selectedProf">
-      <div class="col-12 col-md-6 row" style="border: solid red">
-        <div class="col-12 text-h4 q-mb-md">
-          Prof-Bewertungten für  <strong>{{ profStore.selectedProf.fname }}
-          {{ profStore.selectedProf.lname }}</strong>
-        </div>
-        <div class="col-12 row items-center q-gutter-sm">
-          <!-- Rating -->
-          <q-rating
-            v-model="profStore.selectedProf.factors[0].gesamt"
-            size="2em"
-            icon="star_border"
-            icon-selected="star"
-            icon-half="star_half"
-            color-half="amber-6"
-            color-selected="amber-6"
-            color="amber-8"
-            readonly
-          />
+      <div class="col-12 col-md-6 row my-fixed-height-2">
+        <div class="col-12 row items-center q-gutter-sm q-mb-lg">
 
-          <!-- Text daneben -->
-          <div class="text-body1">
-            Based on {{ profStore.selectedProf.factors[0].ratings }} ratings
+          <!-- Admin delete Button (falls Rolle == 'admin') -->
+          <div v-if="userStore.loggedIn && userStore.user.role === 'admin'" class="q-mt-md">
+            <q-btn label="Professor löschen" color="negative" @click="confirmAll = true" />
+            <q-dialog v-model="confirmAll">
+              <q-card>
+                <q-card-section class="row items-center">
+                  <div>Willst du diesen Professor wirklich unwiderruflich löschen?</div>
+                </q-card-section>
+                <q-card-actions align="right">
+                  <q-btn flat label="Abbrechen" color="primary" v-close-popup />
+                  <q-btn
+                    flat
+                    label="Löschen"
+                    color="negative"
+                    v-close-popup
+                    @click="deleteProfessor(profStore.selectedProf._id)"
+                  />
+                </q-card-actions>
+              </q-card>
+            </q-dialog>
+          </div>
+
+          <div class="col-12 row">
+            <div class="text-h4 text-bold q-mr-sm">
+              {{ profStore.selectedProf.fname }} {{ profStore.selectedProf.lname }}
+            </div>
+            <q-rating
+              v-model="profStore.selectedProf.factors[0].gesamt"
+              size="2em"
+              icon="star_border"
+              icon-selected="star"
+              icon-half="star_half"
+              color-half="amber-6"
+              color-selected="amber-6"
+              color="amber-8"
+              readonly
+            />
+          </div>
+          <div class="col-12 text-body1">
+            {{$t('profcheck.based_on')}}  <strong>{{ profStore.selectedProf.factors[0].ratings }} </strong> {{$t('profcheck.ratings')}} 
           </div>
         </div>
-      </div>
-      <div class="col-12 col-md-6 row" style="border: solid red"></div>
-    </div>
 
-
-
-
-
-
-    <div class="row q-ma-md" v-if="profStore.selectedProf">
-      <div class="col-12 text-h3 text-center q-mb-sm">
-        {{ profStore.selectedProf.fname }} {{ profStore.selectedProf.lname }}
-      </div>
-      <div class="col-12 text-center q-mb-lg">
-        <q-btn
-          v-if="userStore.loggedIn"
-          :label="$t('profcheck.rate_now')"
-          @click="rateProfessor"
-          style="background-color: #ffee6c"
-        />
-        <div v-else class="text-body1">{{ $t('profcheck.login_to_rate') }}</div>
-      </div>
-
-      <!-- Admin delete Button -->
-      <div v-if="userStore.loggedIn && userStore.user.role === 'admin'" class="col-12 text-center">
-        <q-btn label="Professor löschen" color="negative" @click="confirmAll = true" />
-        <q-dialog v-model="confirmAll">
-          <q-card>
-            <q-card-section class="row items-center">
-              <div>Willst du diesen Professor wirklich unwiderruflich löschen?</div>
-            </q-card-section>
-
-            <q-card-actions align="right">
-              <q-btn flat label="Abbrechen" color="primary" v-close-popup />
-              <q-btn
-                flat
-                label="Löschen"
-                color="negative"
-                v-close-popup
-                @click="deleteProfessor(profStore.selectedProf._id)"
-              />
-            </q-card-actions>
-          </q-card>
-        </q-dialog>
-      </div>
-      <div class="col-12 row" v-for="(factor, i) in profStore.selectedProf.factors" :key="i">
+        <div class="col-12 row items-center q-gutter-sm q-mb-lg"></div>
         <div
-          class="col-12 row justify-center q-gutter-sm"
-          v-if="
-            factor.ratings > 0 &&
-            factor.gesamt !== null &&
-            factor.lerninhahlte !== null &&
-            factor.atmospahre !== null &&
-            factor.benotung !== null &&
-            factor.verfugbarkeit !== null &&
-            factor.empfhelung !== null
-          "
+          class="col-12 col-md-8 row q-mb-lg"
+          v-for="(factor, i) in profStore.selectedProf.factors"
+          :key="i"
         >
-          <q-card class="row items-center bg-amber-2">
-            <q-card-section class="text-center">
-              <div class="text-h3">{{ factor.gesamt.toFixed(2) }}</div>
-              <div class="text-subtitle2">{{ $t('lvPlaner.prof_overall') }}</div>
-            </q-card-section>
-          </q-card>
-          <q-card>
-            <q-card-section class="col-12 row">
-              <div class="col-6 col-sm-4 text-center">
-                <div class="text-h4">{{ factor.lerninhahlte.toFixed(2) }}</div>
+          <div
+            class="row q-col-gutter-sm"
+            v-if="
+              factor.ratings > 0 &&
+              factor.gesamt !== null &&
+              factor.lerninhahlte !== null &&
+              factor.atmospahre !== null &&
+              factor.benotung !== null &&
+              factor.verfugbarkeit !== null &&
+              factor.empfhelung !== null
+            "
+          >
+            <div class="col-6 col-md-4">
+              <q-card class="text-center q-pa-sm">
+                <div class="text-h4 text-bold">{{ factor.lerninhahlte.toFixed(2) }}</div>
                 <div class="text-subtitle2">{{ $t('lvPlaner.prof_learning_content') }}</div>
-              </div>
-              <div class="col-6 col-sm-4 text-center">
-                <div class="text-h4">{{ factor.atmospahre.toFixed(2) }}</div>
+              </q-card>
+            </div>
+            <div class="col-6 col-md-4">
+              <q-card class="text-center q-pa-sm">
+                <div class="text-h4 text-bold">{{ factor.atmospahre.toFixed(2) }}</div>
                 <div class="text-subtitle2">{{ $t('lvPlaner.prof_atmosphere') }}</div>
-              </div>
-              <div class="col-6 col-sm-4 text-center">
-                <div class="text-h4">{{ factor.benotung.toFixed(2) }}</div>
+              </q-card>
+            </div>
+            <div class="col-6 col-md-4">
+              <q-card class="text-center q-pa-sm">
+                <div class="text-h4 text-bold">{{ factor.mitarbeit.toFixed(2) }}</div>
                 <div class="text-subtitle2">{{ $t('lvPlaner.prof_participation') }}</div>
-              </div>
-              <div class="col-6 col-sm-4 text-center">
-                <div class="text-h4">{{ factor.verfugbarkeit.toFixed(2) }}</div>
+              </q-card>
+            </div>
+            <div class="col-6 col-md-4">
+              <q-card class="text-center q-pa-sm">
+                <div class="text-h4 text-bold">{{ factor.benotung.toFixed(2) }}</div>
                 <div class="text-subtitle2">{{ $t('lvPlaner.prof_grading') }}</div>
-              </div>
-              <div class="col-6 col-sm-4 text-center">
-                <div class="text-h4">{{ factor.empfhelung.toFixed(2) }}</div>
+              </q-card>
+            </div>
+            <div class="col-6 col-md-4">
+              <q-card class="text-center q-pa-sm">
+                <div class="text-h4 text-bold">{{ factor.verfugbarkeit.toFixed(2) }}</div>
+                <div class="text-subtitle2">{{ $t('lvPlaner.prof_availability') }}</div>
+              </q-card>
+            </div>
+            <div class="col-6 col-md-4">
+              <q-card class="text-center q-pa-sm">
+                <div class="text-h4 text-bold">{{ factor.empfhelung.toFixed(2) }}</div>
                 <div class="text-subtitle2">{{ $t('lvPlaner.prof_recommendation') }}</div>
-              </div>
-              <div class="col-6 col-sm-4 text-center">
-                <div class="text-h4">{{ factor.ratings }}</div>
-                <div class="text-subtitle2">{{ $t('lvPlaner.prof_ratings') }}</div>
-              </div>
-            </q-card-section>
-          </q-card>
-          <div class="col-12 text-h4">{{ $t('lvPlaner.prof_comments') }}:</div>
-          <div class="col-12" v-if="profStore.selectedProf.comments.length > 0">
-            <q-list bordered separator>
-              <q-item v-for="(comment, i) in profStore.selectedProf.comments" :key="i">
-                <q-item-section>
-                  <div v-if="userStore.loggedIn && userStore.user.role === 'admin'">
-                    <q-btn color="negative" label="Löschen" @click="confirm = true" />
-                  </div>
-                  <div class="text-caption">{{ formatDate(comment.date) }}</div>
-                  <div class="text-body1">{{ comment.text }}</div>
-                  <!-- Dialog für Kommentar löschen -->
-                  <q-dialog v-model="confirm">
-                    <q-card>
-                      <q-card-section class="row items-center">
-                        <div>Willst du dieses Kommentar wirklich unwiderruflich löschen?</div>
-                      </q-card-section>
-
-                      <q-card-actions align="right">
-                        <q-btn flat label="Abbrechen" color="primary" v-close-popup />
-                        <q-btn
-                          flat
-                          label="Löschen"
-                          color="negative"
-                          v-close-popup
-                          @click="deleteComment(comment)"
-                        />
-                      </q-card-actions>
-                    </q-card>
-                  </q-dialog>
-                </q-item-section>
-              </q-item>
-            </q-list>
-          </div>
-          <div class="col-12" v-else>
-            <div class="text-center text-h5">{{ $t('profcheck.no_comments') }}</div>
+              </q-card>
+            </div>
           </div>
         </div>
-        <div class="col-12" v-else>
-          <div class="text-center text-h5">{{ $t('profcheck.no_ratings') }}</div>
+        <div class="col-12 column">
+          <div class="text-h6">{{$t('profcheck.share_your_experience')}} </div>
+          <div style="max-width: 380px">
+            <div class="text-body1 q-mb-md">
+              {{$t('profcheck.share_your_experience_text1')}}  <br />
+              {{$t('profcheck.share_your_experience_text2')}}
+            </div>
+            <div>
+              <q-btn
+                class="full-width"
+                v-if="userStore.loggedIn"
+                :label="$t('profcheck.rate_now')"
+                @click="rateProfessor"
+                color="blue-4"
+                outline
+              />
+              <div v-else class="text-body1 text-bold">{{ $t('profcheck.login_to_rate') }}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-md-6 row my-fixed-height">
+        <div class="col-12" v-if="profStore.selectedProf.comments.length > 0">
+          <q-list separator>
+            <q-item
+              v-for="(comment, i) in profStore.selectedProf.comments"
+              :key="i"
+              class="q-mb-lg"
+            >
+              <q-item-section>
+                <!-- Admin Delete Button -->
+                <div v-if="userStore.loggedIn && userStore.user.role === 'admin'">
+                  <q-btn color="negative" label="Löschen" @click="confirm = true" />
+                </div>
+
+                <div class="text-caption q-mt-lg">{{ formatDate(comment.date) }}</div>
+                <div class="text-body1">{{ comment.text }}</div>
+              </q-item-section>
+            </q-item>
+          </q-list>
+          <!-- Dialog für Kommentar löschen -->
+          <q-dialog v-model="confirm">
+            <q-card>
+              <q-card-section class="row items-center">
+                <div>Willst du dieses Kommentar wirklich unwiderruflich löschen?</div>
+              </q-card-section>
+
+              <q-card-actions align="right">
+                <q-btn flat label="Abbrechen" color="primary" v-close-popup />
+                <q-btn
+                  flat
+                  label="Löschen"
+                  color="negative"
+                  v-close-popup
+                  @click="deleteComment(comment)"
+                />
+              </q-card-actions>
+            </q-card>
+          </q-dialog>
         </div>
       </div>
     </div>
@@ -227,4 +233,29 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped>
+/* Globale oder scoped CSS */
+.my-fixed-height {
+  max-height: 600px;
+  overflow-y: auto;
+}
+
+/* Auf Handy-Breite (max 600px) setzen wir die Höhe wieder zurück */
+@media (max-width: 600px) {
+  .my-fixed-height {
+    max-height: none;
+  }
+}
+
+/* Globale oder scoped CSS */
+.my-fixed-height-2 {
+  max-height: 400px;
+}
+
+/* Auf Handy-Breite (max 600px) setzen wir die Höhe wieder zurück */
+@media (max-width: 600px) {
+  .my-fixed-height-2 {
+    max-height: none;
+  }
+}
+</style>
